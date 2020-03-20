9806aa16d44568fe661d7eb5ea46811fb82ace80
Francesco Landolfi <flandolfi@users.noreply.github.com>
2020-03-20 11:32:25 +0100

    Add k-hop orderings

--- gpool/orderings.py
+++ gpool/orderings.py
@@ -1,11 +1,14 @@
 import torch
+import torch_sparse
 from torch_geometric.data import Data
-from torch_geometric.utils import degree
 
 
 class Ordering:
+    def __init__(self, descending=False):
+        self.descending = descending
+
     def __call__(self, data: Data):
-        return data
+        raise NotImplementedError
 
 
 class Random(Ordering):
@@ -15,15 +18,45 @@ class Random(Ordering):
 
 class Canonical(Ordering):
     def __call__(self, data: Data):
-        return torch.arange(data.num_nodes)
+        return torch.arange(data.num_nodes)[::(-1)**int(self.descending)]
 
 
-class Degree(Ordering):
-    def __init__(self, descending=False):
-        self.descending = (-1)**int(descending)
+class InverseCanonical(Canonical):
+    def __init__(self):
+        super(InverseCanonical, self).__init__(True)
+
+
+class KHopDegree(Ordering):
+    def __init__(self, k=1, descending=False):
+        super(KHopDegree, self).__init__(descending)
+        self.k = k
 
     def __call__(self, data: Data):
-        return self.descending * degree(data.edge_index[0], data.num_nodes)
+        out = torch.ones([data.num_nodes, 1], dtype=torch.float)
+        ind, val, n = data.edge_index, data.edge_attr, data.num_nodes
+
+        if val is None:
+            val = torch.ones_like(ind[0], dtype=torch.float)
+
+        for _ in range(self.k):
+            out = torch_sparse.spmm(ind, val, n, out)
+
+        return out * (-1)**int(self.descending)
+
+
+class MinKHopDegree(KHopDegree):
+    def __init__(self, k=1):
+        super(MinKHopDegree, self).__init__(k, False)
+
+
+class MaxKHopDegree(KHopDegree):
+    def __init__(self, k=1):
+        super(MaxKHopDegree, self).__init__(k, True)
+
+
+class Degree(KHopDegree):
+    def __init__(self, descending=False):
+        super(Degree, self).__init__(1, descending)
 
 
 class MinDegree(Degree):
