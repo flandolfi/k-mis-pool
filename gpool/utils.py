64b9a77ff10b7c93ad1224e7714f54534a33bb37
Francesco Landolfi <flandolfi@users.noreply.github.com>
2020-03-20 11:31:58 +0100

    Add sparse k-hop neighbors implementation

--- gpool/utils.py
+++ gpool/utils.py
@@ -1,4 +1,5 @@
 import torch
+import torch_sparse
 
 
 def sparse_sort(src: torch.Tensor, index: torch.Tensor, dim=0, descending=False, eps=1e-12):
@@ -27,3 +28,35 @@ def sparse_min(src: torch.Tensor, index: torch.Tensor, dim=0, eps=1e-12):
 def sparse_max(src: torch.Tensor, index: torch.Tensor, dim=0, eps=1e-12):
     return sparse_topk(src, index, 1, dim, True, eps)
 
+
+def k_hop(edge_index, edge_attr=None, k=2, num_nodes=None, mask=None):
+    n = num_nodes if num_nodes else edge_index.max().item() + 1
+
+    if edge_attr is None:
+        edge_attr = torch.ones_like(edge_index[0], dtype=torch.float)
+
+    adj_k = {1: (edge_index, edge_attr)}
+
+    def adj_pow(exp, in_mask=None, out_mask=None):
+        if exp in adj_k:
+            idx, val = adj_k[exp]
+            row, col = idx
+
+            edge_mask = torch.ones_like(row, dtype=torch.uint8)
+
+            if in_mask is not None:
+                edge_mask &= in_mask[row]
+
+            if out_mask is not None:
+                edge_mask &= out_mask[col]
+
+            return idx[:, edge_mask], val[edge_mask]
+
+        l_exp = exp // 2
+        l_ind, l_val = adj_pow(l_exp, in_mask, None)
+        r_ind, r_val = adj_pow(exp - l_exp, None, out_mask)
+        adj_k[exp] = torch_sparse.spspmm(l_ind, l_val, r_ind, r_val, n, n, n)
+
+        return adj_k[exp]
+
+    return adj_pow(k, mask, mask)
