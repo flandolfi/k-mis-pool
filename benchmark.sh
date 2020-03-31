4d7ff2647608f394afa872e4dc31dcce509246f4
Francesco Landolfi <flandolfi@users.noreply.github.com>
2020-03-31 20:38:34 +0200

    Typo

--- benchmark.sh
+++ benchmark.sh
@@ -4,7 +4,7 @@ DATASET=$1
 DIR=./results/${DATASET}/
 
 shift
-mkdirs -p $DIR
+mkdir -p $DIR
 
 for ORDER in 'min-d-paths' 'max-d-paths' 'min-s-paths' 'max-s-paths' 'min-degree' 'max-degree' 'random'; do
     python -m benchmark.cv -d $DATASET -o $ORDER --to_pickle ${DIR}/${DATASET}_${ORDER}.pickle $@
