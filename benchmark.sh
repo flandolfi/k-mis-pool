c4d05a4b761c081cecf93c0d5d279e99265dfc9b
Francesco Landolfi <flandolfi@users.noreply.github.com>
2020-05-01 12:00:32 +0200

    Change default settings

--- benchmark.sh
+++ benchmark.sh
@@ -6,6 +6,6 @@ DIR=./results/${DATASET}/
 shift
 mkdir -p $DIR
 
-for ORDER in 'min-s-paths' 'max-s-paths' 'min-degree' 'max-degree' 'random'; do
+for ORDER in 'max-s-paths' 'max-p-paths' 'max-3-paths' 'random'; do
     python -m benchmark.cv -d $DATASET -o $ORDER --to_pickle ${DIR}/${DATASET}_${ORDER}.pickle $@
 done
