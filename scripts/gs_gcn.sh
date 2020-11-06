3b47827170ed25d76bc342834daabe89be888d48
Francesco Landolfi <flandolfi@users.noreply.github.com>
2020-11-06 18:46:03 +0100

    Fix scripts

--- scripts/gs_gcn.sh
+++ scripts/gs_gcn.sh
@@ -10,9 +10,9 @@ ORDER=$2
 
 mkdir -p $DIR
 
-python -m benchmarks.benchmark grid_search --model_name=GCN --dataset_name=$DS \
+python -m benchmark.benchmarks grid_search --model_name=GCN --dataset_name=$DS \
     --cv_results_path=$DIR/${GNN}_L2_${DS}_${ORDER}_GS.csv --batch_size=128 --lr=0.001 \
     --module__blocks=2 --module__hidden=104 --module__incremental=False --module__ordering=$ORDER $@
-python -m benchmarks.benchmark grid_search --model_name=GCN --dataset_name=$DS \
+python -m benchmark.benchmarks grid_search --model_name=GCN --dataset_name=$DS \
     --cv_results_path=$DIR/${GNN}_L3_${DS}_${ORDER}_GS.csv --batch_size=128 --lr=0.001 \
     --module__blocks=3 --module__hidden=85 --module__incremental=False --module__ordering=$ORDER $@
