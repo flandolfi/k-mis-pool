d851211522d94b9fb6a3e50662eea74575aad205
Francesco Landolfi <flandolfi@users.noreply.github.com>
2021-03-09 17:31:57 +0100

    Fix skorch/pyg cooperation

--- benchmark/eval.py
+++ benchmark/eval.py
@@ -5,32 +5,16 @@ import numpy as np
 
 import torch
 
-from torch_geometric.data import DataLoader, Data
+from torch_geometric.data import DataLoader, Data, Dataset
 from torch_geometric.datasets import ModelNet
 from torch_geometric import transforms as T
 
 import skorch
 from skorch import NeuralNetClassifier
 from skorch.callbacks import ProgressBar, Checkpoint, EpochScoring
-from skorch.dataset import CVSplit, Dataset, check_indexing
+from skorch.dataset import CVSplit
 
 from benchmark.models import GNN
-
-
-class SkorchDataset(Dataset):
-    def __init__(self, X, y=None, transform=None):
-        super(SkorchDataset, self).__init__(list(X))
-        self.y = torch.cat([data.y for data in self.X])
-        self.y_indexing = check_indexing(self.y)
-        self.transform = transform
-        
-        self._len = len(X)
-
-    def __len__(self):
-        return self._len
-
-    def __getitem__(self, i):
-        return self.transform(self.X[i]), self.y[i]
 
 
 def _to_tensor_wrapper(func):
@@ -45,6 +29,30 @@ def _to_tensor_wrapper(func):
     return wrapper
 
 
+def _get_item_wrapper(func):
+    wrapped = func
+
+    def wrapper(dataset, idx):
+        if isinstance(idx, np.int64):
+            idx = int(idx)
+
+        return wrapped(dataset, idx)
+
+    return wrapper
+
+
+def _unpack_data_wrapper(func):
+    wrapped = func
+
+    def wrapper(data):
+        if isinstance(data, Data):
+            return data, data.y
+
+        return wrapped(data)
+
+    return wrapper
+
+
 torch.manual_seed(42)
 np.random.seed(42)
 device = 'cpu'
@@ -53,7 +61,10 @@ if torch.cuda.is_available():
     torch.cuda.manual_seed_all(42)
     device = 'cuda'
 
+skorch.net.unpack_data = _unpack_data_wrapper(skorch.net.unpack_data)
 skorch.net.to_tensor = _to_tensor_wrapper(skorch.net.to_tensor)
+Dataset.__getitem__ = _get_item_wrapper(Dataset.__getitem__)
+
 warnings.filterwarnings("ignore", category=UserWarning)
 
 
@@ -67,7 +78,6 @@ DEFAULT_NET_PARAMS = {
     'lr': 0.001,
     'batch_size': -1,
     'criterion': torch.nn.CrossEntropyLoss,
-    'dataset': SkorchDataset,
     'iterator_train': DataLoader,
     'iterator_valid': DataLoader,
     'iterator_train__shuffle': True,
@@ -82,7 +92,7 @@ def modelnet(root: str = './dataset/ModelNet40/',
              model_path: str = None,
              history_path: str = None,
              **net_kwargs):
-    ds = ModelNet(root, '40', train=True)
+    ds = ModelNet(root, '40', train=True, transform=T.FixedPoints(num=num_nodes, replace=False, allow_duplicates=False))
     ds.data.face = None
     ds.slices.pop('face')
     
@@ -97,7 +107,6 @@ def modelnet(root: str = './dataset/ModelNet40/',
                 lower_is_better=False)),
             ('checkpoint', Checkpoint),
         ],
-        'dataset__transform': T.FixedPoints(num=num_nodes, replace=False, allow_duplicates=False),
         'train_split': CVSplit(cv=0.1, stratified=True, random_state=42),
         'callbacks__checkpoint__monitor': 'valid_acc_best',
         'callbacks__checkpoint__f_params': model_path,
@@ -113,7 +122,7 @@ def modelnet(root: str = './dataset/ModelNet40/',
         **opts
     )
 
-    net.fit(list(ds), ds.data.y)
+    net.fit(ds, ds.data.y)
 
 
 if __name__ == "__main__":
