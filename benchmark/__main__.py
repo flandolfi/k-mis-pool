4b518f867f130d4eb3fea81d1a8298257c687595
Francesco Landolfi <flandolfi@users.noreply.github.com>
2021-04-27 14:13:30 +0200

    Refactor

--- benchmark/__main__.py
+++ benchmark/__main__.py
@@ -1,13 +1,13 @@
 import fire
 
-from benchmark.spectrum import approx, spectrum_approximation
+from benchmark.spectrum import approx
 from benchmark.grids import draw_mnist
 from benchmark.sample import sample
 
 
 if __name__ == "__main__":
     fire.Fire({
-        'approx': spectrum_approximation,
+        'approx': approx,
         'mnist': draw_mnist,
         'sample': sample,
     })
