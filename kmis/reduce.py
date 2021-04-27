7a47a6d6a71df73b0abf87f78fbc425b5e0c2da7
Francesco Landolfi <flandolfi@users.noreply.github.com>
2021-04-27 14:10:59 +0200

    Fix coarsening

--- kmis/reduce.py
+++ kmis/reduce.py
@@ -18,12 +18,12 @@ def get_coarsening_matrix(adj: SparseTensor, k: int = 1, eps: float = 0.5,
         diag = rw.get_diag() + eps
         rw = rw.set_diag(diag)
     
-    c_mat = rw.eye(adj.size(0), device=adj.device())[:, mis]
+    c_mat = rw.eye(adj.size(0), device=adj.device())[mis]
     
     for _ in range(k):
-        c_mat = rw @ c_mat
+        c_mat = c_mat @ rw
     
-    return utils.normalize_dim(c_mat), mis
+    return utils.normalize_dim(c_mat.t()), mis
 
 
 def sample_partition_matrix(c_mat: SparseTensor) -> SparseTensor:
@@ -92,20 +92,19 @@ class KMISCoarsening(torch.nn.Module):
         return getattr(orderings, cls_name)(**opts)
 
     @staticmethod
-    def pool(p_inv: Union[Tensor, SparseTensor], *xs: OptTensor) -> Tuple[OptTensor, ...]:
+    def pool(l_mat: Union[Tensor, SparseTensor], *xs: OptTensor) -> Tuple[OptTensor, ...]:
         out = []
 
         for x in xs:
             if x is None:
                 out.append(None)
             else:
-                out.append(p_inv @ x)
+                out.append(l_mat @ x)
                 
         return tuple(out)
 
     @staticmethod
-    def coarsen(c_mat: Union[Tensor, SparseTensor], adj: SparseTensor) \
-            -> Union[Tensor, SparseTensor]:
+    def coarsen(c_mat: Union[Tensor, SparseTensor], adj: SparseTensor) -> Union[Tensor, SparseTensor]:
         return c_mat.t() @ (adj @ c_mat)
 
     @staticmethod
@@ -124,25 +123,21 @@ class KMISCoarsening(torch.nn.Module):
         return self.ordering(x, adj)
 
     def get_coarsening_matrices(self, adj: SparseTensor, x: OptTensor = None) \
-            -> Tuple[SparseTensor, Union[Tensor, SparseTensor], Tensor]:
+            -> Tuple[SparseTensor, SparseTensor, Tensor]:
         rank = self._get_rank(x, adj)
         c_mat, mis = get_coarsening_matrix(adj, self.k, self.eps, rank)
 
         if self.sample_partition:
             c_mat = sample_partition_matrix(c_mat)
-            p_inv = utils.normalize_dim(c_mat.t())
-        else:
-            p_inv = c_mat.to_dense()
-            p_inv = torch.pinverse(p_inv)
+        
+        l_mat = utils.normalize_dim(c_mat.t())
 
-        return c_mat, p_inv, mis
+        return c_mat, l_mat, mis
 
     def forward(self, x: Union[OptTensor, OptPairTensor],
                 edge_index: Adj, edge_attr: OptTensor = None,
-                batch: OptTensor = None) -> Tuple[Union[OptTensor, OptPairTensor],
-                                                  SparseTensor, OptTensor,
-                                                  Tensor, SparseTensor,
-                                                  Union[Tensor, SparseTensor]]:
+                batch: OptTensor = None) -> Tuple[Union[OptTensor, OptPairTensor], SparseTensor,
+                                                  OptTensor, Tensor, SparseTensor, SparseTensor]:
         if not isinstance(x, tuple):
             x: Tuple[Tensor] = (x,)
 
@@ -152,10 +147,10 @@ class KMISCoarsening(torch.nn.Module):
         if isinstance(adj, Tensor):
             adj = SparseTensor.from_edge_index(edge_index, edge_attr, size)
 
-        c_mat, p_inv, mis = self.get_coarsening_matrices(adj, x[0])
+        c_mat, l_mat, mis = self.get_coarsening_matrices(adj, x[0])
 
         adj = self.coarsen(c_mat, adj)
-        out = self.pool(p_inv, *x)
+        out = self.pool(l_mat, *x)
 
         if len(out) == 1:
             out = out[0]
@@ -163,4 +158,4 @@ class KMISCoarsening(torch.nn.Module):
         if batch is not None:
             batch = batch[mis]
         
-        return out, adj, batch, mis, c_mat, p_inv
+        return out, adj, batch, mis, c_mat, l_mat
