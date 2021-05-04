a676a3fcdaee3643b317d0579d9c67cc40a4e732
Francesco Landolfi <flandolfi@users.noreply.github.com>
2021-05-04 13:04:22 +0200

    Switch to custom approx benchmarks

--- benchmark/__main__.py
+++ benchmark/__main__.py
@@ -1,13 +1,13 @@
 import fire
 
-from benchmark.spectrum import approx
+from benchmark.approx import spectrum_approximation
 from benchmark.grids import draw_mnist
 from benchmark.sample import sample
 
 
 if __name__ == "__main__":
     fire.Fire({
-        'approx': approx,
+        'approx': lambda *args, **kwargs: str(spectrum_approximation(*args, **kwargs)),
         'mnist': draw_mnist,
         'sample': sample,
     })
