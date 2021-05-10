31398b081da464352d524e88eccac45f76b81ec8
Francesco Landolfi <flandolfi@users.noreply.github.com>
2021-05-10 17:15:24 +0200

    Fix scripts

--- scripts/train_baselines.sh
+++ scripts/train_baselines.sh
@@ -4,12 +4,16 @@ RESULTS_DIR="./results/"
 mkdir -p $RESULTS_DIR
 
 for DS in MNIST CIFAR10 CLUSTER PATTERN; do
+    DIR="${RESULTS_DIR}/${DS}/"
+    mkdir -p $DIR
+
     for MODEL in GCN ChebNet GraphSAGE; do
         for PARAMS in 100K 500K; do
             NAME="${MODEL}_${PARAMS}"
+
             python -m benchmark train --model $NAME --dataset $DS \
-                --save_params ${RESULTS_DIR}/${NAME}.pt \
-                --save_history ${RESULTS_DIR}/${NAME}.json \
+                --save_params ${DIR}/${NAME}.pt \
+                --save_history ${DIR}/${NAME}.json \
                 --batch_size 128 --num_workers 8 $@
         done
     done
--- scripts/train_pool.sh
+++ scripts/train_pool.sh
@@ -3,15 +3,19 @@
 RESULTS_DIR="./results/"
 mkdir -p $RESULTS_DIR
 
-for DS in PATTERN CLUSTER; do
+for DS in MNIST CIFAR10 PATTERN CLUSTER; do
+    DIR="${RESULTS_DIR}/${DS}/"
+    mkdir -p $DIR
+
     for MODEL in GCN ChebNet GraphSAGE; do
         for PARAMS in 100K 500K; do
             for ORDER in "random" "min-local-variation"; do
                 NAME="${MODEL}_P_${PARAMS}"
+
                 python -m benchmark train --model $NAME --dataset $DS \
                     --module__ordering $ORDER \
-                    --save_params ${RESULTS_DIR}/${NAME}_${ORDER}.pt \
-                    --save_history ${RESULTS_DIR}/${NAME}_${ORDER}.json \
+                    --save_params ${DIR}/${NAME}.pt \
+                    --save_history ${DIR}/${NAME}.json \
                     --batch_size 128 --num_workers 8 $@
             done
         done
