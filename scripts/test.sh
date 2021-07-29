c1b9d172000991bb9134f032e0e9109326ae77d5
Francesco Landolfi <flandolfi@users.noreply.github.com>
2021-07-29 18:01:09 +0200

    Update test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 DIR="./results/"
-echo "dataset,model,accuracy"
+echo "dataset,model,k,accuracy"
 
 for FILE in `ls ${DIR}/*/*.pt`; do
     NAME="${FILE%__*}"
+    K="${NAME#*_K}"
     NAME="${NAME#$DIR/*}"
     MODEL="${NAME##*/}"
+    MODEL="${MODEL%_K*}"
     DS="${NAME%/*}"
 
-    echo -n "${DS},${MODEL},"
+    echo -n "${DS},${MODEL%_44*},${K},"
     python -m benchmark score --model $MODEL --dataset $DS --params_path $FILE \
         --batch_size 128 $@ | tail -n 1
 done
