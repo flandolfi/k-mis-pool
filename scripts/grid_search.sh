c6e95b6f3c156fb8a816b0b38b4c2388337633d6
Francesco Landolfi <flandolfi@users.noreply.github.com>
2022-01-25 03:05:58 +0100

    Clean trial files

--- scripts/grid_search.sh
+++ scripts/grid_search.sh
@@ -1,21 +1,37 @@
 #!/bin/bash
 
-CONFIG_ALL="--root datasets --cpu_per_trial 4 --gpu_per_trial 0.5"
+RESULTS_DIR="./results/"
+CONFIG_ALL="--root datasets --local_dir $RESULTS_DIR --cpu_per_trial 4 --gpu_per_trial 0.5"
 GS_CMD="python -m benchmark grid_search $CONFIG_ALL"
+
+clean_up () {
+  PREFIX=$RESULTS_DIR/${1}_${2}
+  mv $PREFIX/model_assessment.json ${PREFIX}.json
+  rm -r $PREFIX
+}
 
 for DATASET in "DD" "REDDIT-BINARY" "REDDIT-MULTI-5K" "REDDIT-MULTI-12K"; do
   for MODEL in Baseline GraclusPool; do
     $GS_CMD --dataset $DATASET --model $MODEL
+
+    clean_up $MODEL $DATASET
   done
 
   for MODEL in TopKPool SAGPool ASAPool; do
     $GS_CMD --dataset $DATASET --model $MODEL --opt_grid "{'ratio':[0.5,0.2,0.1]}"
+
+    clean_up $MODEL $DATASET
   done
 
   for MODEL in KMISPool KMISPoolRandom KMISPoolNorm; do
     $GS_CMD --dataset $DATASET --model $MODEL --opt_grid "{'k':[1,2,3]}"
+
+    clean_up $MODEL $DATASET
   done
 
-  $GS_CMD --dataset $DATASET --model PANPool \
+  MODEL=PANPool
+  $GS_CMD --dataset $DATASET --model $MODEL \
     --opt_grid "{'ratio':[0.5,0.2,0.1],'filter_size':[1,2,3],'gnn_class':['PANConv']}"
+
+  clean_up $MODEL $DATASET
 done
