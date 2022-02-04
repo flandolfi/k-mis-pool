09f934f15d8b441a95dbc498c22f8a49a9533852
Francesco Landolfi <flandolfi@users.noreply.github.com>
2022-02-04 21:14:23 +0100

    Fix profiling, pt. VI

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-CMD="python -m benchmark profile"
+CMD="python -m benchmark profile --device cuda"
 OUT_DIR="./results/stats/"
 mkdir -p $OUT_DIR
 
 KS="'[1,2,4,8]'"
 GROUP="SNAP"
 for DS in com-Friendster com-Orkut com-Youtube com-LiveJournal as-Skitter; do
-    $CMD --name $DS --group $GROUP --k $KS --device --output_json $OUT_DIR/${DS}.json
+    $CMD --name $DS --group $GROUP --k $KS --output_json $OUT_DIR/${DS}.json
 done
 
 GROUP="DIMACS10"
 for DS in coPapersDBLP coPapersCiteseer; do
-    $CMD --name $DS --group $GROUP --k $KS --device --output_json $OUT_DIR/${DS}.json
+    $CMD --name $DS --group $GROUP --k $KS --output_json $OUT_DIR/${DS}.json
 done
 
 KS="'[1,10,100,1000]'"
 for DS in europe_osm asia_osm italy_osm; do
-    $CMD --name $DS --group $GROUP --k $KS --device --output_json $OUT_DIR/${DS}.json
+    $CMD --name $DS --group $GROUP --k $KS --output_json $OUT_DIR/${DS}.json
 done
